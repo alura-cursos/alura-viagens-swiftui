764026b00b93421436501b4143fa744f4e2be2b7
Ândriu <andriu_coelho1993@hotmail.com>
2020-03-03 14:00:09 -0300

    #aula2

--- alura-viagens/ContentView.swift
+++ alura-viagens/ContentView.swift
@@ -10,11 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        
+        GeometryReader { view in
+            
+            // MARK: - VStack Principal
+            
             VStack {
-                Text("Hello, World!")
+                
+                // MARK: - VStack Header
+                
+                VStack {
+                    Text("alura viagens")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 50)
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(/*@START_MENU_TOKEN@*/Color.purple/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(.all)
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("Rio de Janeiro")
+                }
             }
-            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
         }
     }
 }
@@ -24,7 +55,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-// UIKit: Storyboard -> XML -> ViewController
-
-// Swift: View
