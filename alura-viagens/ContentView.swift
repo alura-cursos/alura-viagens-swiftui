bcbe6ce1cb132564a119eeb03ba0672b1016f795
Ândriu <andriu_coelho1993@hotmail.com>
2020-03-03 13:58:58 -0300

    #aula1

---
+++ alura-viagens/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  alura-viagens
+//
+//  Created by Ândriu Felipe Coelho on 03/02/20.
+//  Copyright © 2020 Ândriu Felipe Coelho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                Text("Hello, World!")
+            }
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+// UIKit: Storyboard -> XML -> ViewController
+
+// Swift: View
